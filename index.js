c7840ccff80c637ee5db4cbb2f8b4134e5108018
Joshua <scott.joshua@gmail.com>
2017-12-08 17:17:15 -0700

    inital commit

--- index.js
+++ index.js
@@ -1,7 +1,85 @@
 'use strict';
 
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
-const server = awsServerlessExpress.createServer(app)
+const AWS = require("aws-sdk");
+AWS.config.update({region: "us-west-2"});
+const docClient = new AWS.DynamoDB.DocumentClient();
+const fetch = require("node-fetch");
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+
+exports.handler = (event, context, callback) => {
+
+    let id = event['pathParameters']['id'];
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    getCustomer(id, done);
+};
+
+
+
+const getCustomer = function(id, callback) {
+    docClient.get({
+        TableName: 'Customer',
+        Key: {CustomerID: id}
+    },  function(err, data) {
+        if(!data.Item){
+            loadCustomer(id, function(err, data){
+                if(!err){
+                    getCustomer(id, callback)
+                }else{
+                    callback(err, data); //error loading SKU from SAP
+                }
+            });
+        }else{
+            callback(err, data);
+        }
+    });
+};
+
+
+const loadCustomer = function(id, callback) {
+
+    fetch('https://www.nuskin.com/account-service/api/v1/account/simple/' + id, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' , 'client_id':'735b1eb810304bba966af0891ab54053', 'client_secret':'0deea37049e84157A406C0C01D29F300'},
+    })
+        .then((response) => {
+        if (response.ok) {
+        return response;
+    }
+    return Promise.reject(new Error(
+        `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+})
+.then(response => response.buffer())
+.then((buffer) => {
+
+        const customer = JSON.parse(buffer).SimpleAccount;
+        console.log("customer", customer);
+    docClient.put({
+        TableName: 'Customer',
+        Item: {CustomerID: customer.sapId, FirstName: customer.firstName, LastName: customer.lastName, CustomerType: customer.customerType, email: customer.email ,timestamp: Date.now()}
+    },   function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+            callback(err, data);
+        }
+    });
+}
+)
+};
+
+
+
+
+
+
+
+
