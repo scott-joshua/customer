f9c77ea39f74d29e21e788ef7163985a65112521
Joshua <scott.joshua@gmail.com>
2017-12-13 13:34:55 -0700

    update policy revert back t o standard

--- index.js
+++ index.js
@@ -42,15 +42,37 @@ const loadCustomer = function(id, callback) {
 })
 .then(response => response.buffer())
 .then((buffer) => {
-
         const customer = JSON.parse(buffer).SimpleAccount;
-        console.log("customer", customer);
-    docClient.put({
+
+        let newCustomer = {};
+        newCustomer.CustomerID = customer.sapId;
+        newCustomer.name = customer.lastName+"' "+ customer.firstName;
+        newCustomer.localeName = newCustomer.name;
+        newCustomer.email = customer.email;
+        newCustomer.countryCd = customer.homeMarket;
+        newCustomer.timestamp = Date.now();
+
+        if(customer.customerType === 10){
+            newCustomer.accountType = 10;
+            newCustomer.priceType = "WHL";
+            newCustomer.customerType = "DISTRIBUTOR";
+        } else if(customer.customerType === 20){
+            newCustomer.accountType = 20;
+            newCustomer.priceType = "RTL";
+            newCustomer.customerType = "CUSTOMER";
+        } else if(customer.customerType === 30){
+            newCustomer.accountType = 30;
+            newCustomer.priceType = "WHL";
+            newCustomer.customerType = "PREFFERRED";
+        }
+
+        docClient.put({
         TableName: 'Customer',
-        Item: {CustomerID: customer.sapId, FirstName: customer.firstName, LastName: customer.lastName, CustomerType: customer.customerType, email: customer.email ,timestamp: Date.now()}
+        Item: newCustomer
     },   function(err, data) {
         if (err) {
             console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            callback(err, null);
         } else {
             console.log("Added item:", JSON.stringify(data, null, 2));
             callback(err, data);
